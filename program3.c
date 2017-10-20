6152a8e2d1d8abd38510334cd51ea26ac9dacb28
kumy74 <tomek.pl@poczta.fm>
2017-10-20 14:12:33 +0200

    kalkulator liczb 2 i 5

--- program3.c
+++ program3.c
@@ -4,6 +4,10 @@ int mnoz(int a, int b) { return a * b; }
 
 int dodaj(int a, int b) { return a + b;}
 
+int dziel(int a, int b) {return a / b;}
+
+int odejmij(int a, int b) {return a - b;}
+ 
 
 int main()
 {
@@ -13,10 +17,14 @@ int main()
 		scanf("%c", &wybor); /* to oznacza czytaj z konsoli i zapisz wartosc pod wybor */
 	
 /* if -( wyrazenie){ blok jesli wyrazenie prawdziwe} else {blok jesli wyrazenie fałszywe};   */
-		if(wybor =='*'){; /* == to operator ktory sprawdza czy rowne badz rozne */
+		if(wybor); /* == to operator ktory sprawdza czy rowne badz rozne */
+	        
 		printf("wynik mnozenia: %i\n", mnoz(a,b));
 		}else{printf("wynik dodawania: %i\n", dodaj(a,b));
-		};	
+		}else{printf("Wynik dzielenia: %i\n", podziel(a,b));
+		}else{printf("Wynik odejmowania: %i\n", odejmij(a,b));
+
+	
 		int wynik; /*deklaracja zmiennej typu całkowitego, kompilator nie wie co oznacza*/ 
 		/*wyraz wynik wiec poleceniem int podpowiadasz mu co ma zrobic - wyswietlic wynik*/
 		
