6707453a8ce6a0e036ef2b29019c97ed1cef4c2a
kumy74 <tomek.pl@poczta.fm>
2017-10-20 11:10:33 +0200

    zmiana w programie jesli, to

---
+++ program2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int mnoz(int a, int b) { return a * b; }
+
+int dodaj(int a, int b) { return a + b;}
+
+
+int main()
+{
+		int a = 2, b = 5; /*deklaracja wraz z inicjalizacja */ 
+		char wybor = '*';
+/* if -( wyrazenie){ blok jesli wyrazenie prawdziwe} else {blok jesli wyrazenie fałszywe};   */
+		if(wybor =='*'){; /* == to operator ktory sprawdza czy rowne badz rozne */
+		printf("wynik mnozenia: %i\n", mnoz(a,b));
+		}else{printf("wynik dodawania: %i\n", dodaj(a,b));
+		};	
+		int wynik; /*deklaracja zmiennej typu całkowitego, kompilator nie wie co oznacza*/ 
+		/*wyraz wynik wiec poleceniem int podpowiadasz mu co ma zrobic - wyswietlic wynik*/
+		
+		/*wynik = mnoz(a,b); /*wskazuje co ma zrobic, pomnozyc 4 i 5 */
+
+		/*printf("wynik mnozenia: %i\n", wynik);/* wyswietla wynik dzialania  - mnozenia */
+
+
+
+
+
+
+
+		return 0;
+
+
+
+
+}
