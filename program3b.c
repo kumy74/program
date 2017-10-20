a98a2420f39c284a5545339dad91510ec4fd5051
kumy74 <tomek.pl@poczta.fm>
2017-10-20 14:26:42 +0200

    kalkulator z różnymi argumentami

---
+++ program3b.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int mnoz(int a, int b) {
+		 printf("Wynik mnozenia: %i\n", a * b);
+		 return a * b ;}
+int dodaj(int a, int b) {
+		 printf("Wynik dodawania: %i\n", a + b);
+		 return a + b;}
+int dziel(int a, int b) {
+		 printf("Wynik dzielenia: %i\n", a / b);
+		 return a / b;}
+int odejmij(int a, int b) {
+		 printf("Wynik odejmowania: %i\n", a - b);
+		 return a - b;} 
+
+int main()
+{
+		int a = 2, b = 5;  
+		char wybor;
+
+		printf("Podaj wartosc argumentu a: "); scanf("%i", &a);
+		printf("Podaj wartosc argumentu b: "); scanf("%i", &b);
+		printf("Podaj dzialanie: "); scanf("%c", &wybor);	
+
+		
+		scanf("%c", &wybor); 
+	
+		if(wybor =='*')
+		{mnoz(a,b);}
+
+		else if(wybor =='+')
+		{dodaj(a,b);}
+
+		else if(wybor =='/')
+		{dziel(a,b);}
+
+		else if(wybor =='-')
+		{odejmij(a,b);}; 
+
+	
+		return 0;
+
+}
